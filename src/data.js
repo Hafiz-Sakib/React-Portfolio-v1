fc4ba9c0a543f51b747ad81df27970b1f7ff844a
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-05-20 23:07:08 +0600

    Done

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    link: "",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "",
+    link: "http://lama.dev",
+  },
+];
